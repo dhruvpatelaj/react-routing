c5a0fcadc0a639f895f8597561943e8ee88f291b
Dhruv Patel <dhruvpatel.aj@gmail.com>
2020-06-10 22:29:34 -0600

    fetch calls for api routing

--- src/Nav.js
+++ src/Nav.js
@@ -4,14 +4,18 @@ import "./App.css";
 import { Link } from "react-router-dom";
 
 function Nav() {
+  const navStyle = {
+    color: "white",
+  };
+
   return (
     <nav>
       <h3> LOGO </h3>
       <ul className="nav-links">
-        <Link>
+        <Link style={navStyle} to="/about">
           <li> About </li>
         </Link>
-        <Link>
+        <Link style={navStyle} to="/shop">
           <li> Shop </li>
         </Link>
       </ul>
--- src/Shop.js
+++ src/Shop.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 
+//* useEffect runs the fetch call
 function Shop() {
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  const fetchItems = async () => {
+    const data = await fetch(
+      "https://fortnite-api.theapinetwork.com/upcoming/get",
+      {
+        headers: {
+          Authorization: "{{authorization}}",
+        },
+      }
+    );
+    const items = await data.json();
+    console.log(items);
+  };
   return (
     <div>
       <h1> Shop Page </h1>
